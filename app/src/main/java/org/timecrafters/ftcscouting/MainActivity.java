5ffedc683e906a137d53abf75ac283e28c627ba3
Cyberarm <matthewlikesrobots@gmail.com>
2017-01-24 20:56:13 -0600

    Added ScoutTeamAutonomous activity

--- app/src/main/java/org/timecrafters/ftcscouting/MainActivity.java
+++ app/src/main/java/org/timecrafters/ftcscouting/MainActivity.java
@@ -3,10 +3,9 @@ package org.timecrafters.ftcscouting;
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.util.SparseArray;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -32,6 +31,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         MainActivityContext = this;
+
+        Button scoutTeam = (Button) findViewById(R.id.scout_team);
+        scoutTeam.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(getBaseContext(), ScoutTeamAutonomousActivity.class));
+            }
+        });
 
         Button scoutMatch = (Button) findViewById(R.id.scout_match);
         scoutMatch.setOnClickListener(new View.OnClickListener() {
---
+++ app/src/main/java/org/timecrafters/ftcscouting/ScoutTeamAutonomousActivity.java
@@ -0,0 +1,13 @@
+package org.timecrafters.ftcscouting;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ScoutTeamAutonomousActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scout_team_autonomous);
+    }
+}
